da27a1b29d687607cf397b3c084d96cc82aa9eb7
divineLush <alvipath@yahoo.com>
2021-07-19 04:17:03 -0400

    serving some static files

--- mod.ts
+++ mod.ts
@@ -1,4 +1,4 @@
-import { Application } from "https://deno.land/x/oak@v7.7.0/mod.ts";
+import { Application, send } from "https://deno.land/x/oak@v7.7.0/mod.ts";
 
 const app = new Application();
 const port = 8000;
@@ -17,6 +17,19 @@ app.use(async (context, next) => {
   await next();
   const delta = Date.now() - start;
   context.response.headers.set("X-Response-Time", `${delta}ms`);
+});
+
+app.use(async (context) => {
+  const filePath = context.request.url.pathname;
+  const fileWhitelist = [
+    "/index.html",
+    "/javascripts/script.js",
+    "/stylesheets/style.css",
+    "/images/favicon.png",
+  ];
+  if (fileWhitelist.includes(filePath)) {
+    await send(context, filePath, { root: `${Deno.cwd()}/public` });
+  }
 });
 
 // endpoint
