fa8372f29bb77b33097f0afa7a016f5c474faea5
divineLush <alvipath@yahoo.com>
2021-07-19 21:31:34 +0700

    updated routes

--- api.ts
+++ api.ts
@@ -1,13 +1,17 @@
-import { Router } from "https://deno.land/x/oak@v7.7.0/mod.ts";
+import { Router, send } from "https://deno.land/x/oak@v7.7.0/mod.ts";
 import planets from "./models/planets.ts";
 
 const router = new Router();
-router.get("/", (context) => {
-  context.response.body = "Hello World";
+router.get("/", async (context) => {
+  await send(context, "/index.html", { root: `${Deno.cwd()}/public` });
 });
 
 router.get("/planets", (context) => {
   context.response.body = planets;
+});
+
+router.get("/hello", (context) => {
+  context.response.body = "Hello World";
 });
 
 export default router;
